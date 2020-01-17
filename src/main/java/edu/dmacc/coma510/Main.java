da409e36615acea7d759df268394836e92caa0ce
Gregory Hazen <gmhazen@dmacc.edu>
2020-01-17 14:09:21 -0600

    Added spring and created the container

--- src/main/java/edu/dmacc/coma510/Main.java
+++ src/main/java/edu/dmacc/coma510/Main.java
@@ -1,6 +1,16 @@
 package edu.dmacc.coma510;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration // Tell Spring Main is the configuration class
+@ComponentScan(value = {"edu.dmacc.coma510"}) // Have Spring search for all beans
 public class Main {
     public static void main(String[] args) {
+        //Create our context/IOC container and give it the configuration
+        ApplicationContext context =
+                new AnnotationConfigApplicationContext(Main.class);
     }
 }
